2e321476bcaaab7f171c0b4c0cd9af0da5066882
Drew D <vernthexiii@gmail.com>
2017-08-21 16:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,13 +20,12 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return {
-    myMethod: function() {
+  return function() {
     return "FUNKY!";
-    }
-  };
+  }
 }
 
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // NOTE: To pass this final test, you only need to modify the code below this line.
-Module.myMethod();
+var myFunk = funkyFunction();
+myFunk()();
